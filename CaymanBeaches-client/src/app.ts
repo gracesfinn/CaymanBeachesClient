d9eb1cebf1fc7f7f91301ba10d33c961f5eb3bcc
gracesfinn <graciefinn06@gmail.com>
2020-05-30 16:02:27 -0500

    beaches.ts and beaches.html created and linked to app files

--- CaymanBeaches-client/src/app.ts
+++ CaymanBeaches-client/src/app.ts
@@ -1,16 +1,3 @@
 export class App {
-  name: string;
-  location: string;
-  activities: string;
-  beaches: any[] = [];
 
-  addBeach() {
-    const beach = {
-      name: this.name,
-      location: this.location,
-      activities: this.activities
-    };
-    this.beaches.push(beach);
-    console.log(beach);
-  }
 }
---
+++ CaymanBeaches-client/src/services/cb-types.ts
@@ -0,0 +1,5 @@
+export interface Beach {
+  name: string;
+  location: string;
+  activities: string;
+}
