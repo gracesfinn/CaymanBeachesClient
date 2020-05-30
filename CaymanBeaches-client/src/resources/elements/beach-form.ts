f0c4856eae90668a8b9d8ddaa3f280a0e9925476
gracesfinn <graciefinn06@gmail.com>
2020-05-30 16:20:42 -0500

    beach-form files added

---
+++ CaymanBeaches-client/src/resources/elements/beach-form.ts
@@ -0,0 +1,21 @@
+import { bindable } from 'aurelia-framework';
+import { Beach } from '../../services/cb-types';
+
+export class BeachForm {
+  name: string;
+  location: string;
+  activities: string;
+  @bindable
+  beaches: Beach[];
+
+  addBeach() {
+    const beach = {
+      name: this.name,
+      location: this.location,
+      activities: this.activities
+    };
+    this.beaches.push(beach);
+    console.log(beach);
+  }
+
+}
--- CaymanBeaches-client/src/views/beaches.ts
+++ CaymanBeaches-client/src/views/beaches.ts
@@ -1,18 +1,7 @@
 import { Beach } from '../services/cb-types';
 
 export class Beaches {
-  name: string;
-  location: string;
-  activities: string;
+
   beaches: Beach[] = [];
 
-  addBeach() {
-    const beach = {
-      name: this.name,
-      location: this.location,
-      activities: this.activities
-    };
-    this.beaches.push(beach);
-    console.log(beach);
-  }
 }
